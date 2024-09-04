02e99867795f5376283012124a0e87ea3a6f00ab
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-04 13:40:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!doctype html>
+<?php
+include "view-header.php"
+?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -10,27 +12,4 @@
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
-
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div style="display: flex; flex-direction: row;"class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
-          <a class="nav-link" href="post+result.php">Post Result</a>
-          <a class="nav-link" href="get.php">Get</a>
-          <a class="nav-link" href="get-result.php">Get Result</a>
-
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 </html>
