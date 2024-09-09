f2980c0c2a125433fe565120870bd8792bcc69f6
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-09 13:45:32 -0500

    Update post.php

--- post.php
+++ post.php
@@ -4,6 +4,10 @@ include "view-header.php";
 ?>
 
 <h1>Post</h1>
+<form method="post" action="post-result.php"> 
+  <input type="text" name="my-name">
+  <input type="submit" value="Post data>
+</form>
 
 <?php
 include "view-footer.php";
