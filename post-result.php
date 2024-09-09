43304344645b9ba93ca074fdedaf962e0928f9e1
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-09 14:55:35 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -6,18 +6,18 @@ include "view-header.php";
 <h1>Post result</h1>
 
 <?php
+echo getDisplay();
+include "view-footer.php";
+
+function getDisplay() 
+{
 if (isset($_POST['my-name'])) 
 {
-?>
-  <p>The value sent is:</p>
-<?php
-  echo $_POST['my-name'];
+  return "<p>The value sent is: </p>" . $_POST['my-name'];
 } 
 else
 {
-?>
-  <p>Nothing posted to the page.</p>
-  <?php
+  return "<p>Nothing posted to the page.</p>";
 }
-include "view-footer.php";
+}
 ?>
