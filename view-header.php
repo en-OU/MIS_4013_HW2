9b494f9d647940090494041c55f03c86f087eee3
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-09 13:33:32 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -3,15 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>MIS 4013 HW2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
-
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -24,11 +19,18 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="post.php">Post</a>
+        </li>
+               <li class="nav-item">
+          <a class="nav-link" href="post-result.php">Post Result</a>
+        </li>
+               <li class="nav-item">
+          <a class="nav-link" href="get.php">Get</a>
+        </li>
+               <li class="nav-item">
+          <a class="nav-link" href="get-result.php">Get Result</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-
-
