69357c1db889a92f6a1b6d6fd42c0e26c024c1b2
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-16 00:04:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "view-header.php";
 
 <div style="text-align: center; padding: 50px;">
   <span style="font-weight: bold; color: pink; font-size: 50px; border-style: dashed; border-radius: 10px; border-width: 2px; display: inline-block; padding: 15px;">Home</span>
-  <span style="color: pink; font-size: 20px; display: inline-block; text-align: center; ">Home</span>
+  <span style="color: pink; font-size: 20px; display: inline-block; text-align: center; ">MIS 4013 HW2 - Emily Newby</span>
 </div>
 
 <?php
