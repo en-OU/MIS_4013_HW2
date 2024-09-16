0dd7d922dd47068de62c62617ece2e48158b9073
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-15 23:48:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
-<span style="font-weight: bold; color: pink; font-size: 50px; border-style: dashed; border-radius: 10px; border-color: black; display: inline;">&nbsp;Home&nbsp;</span>
+<span style="font-weight: bold; color: pink; font-size: 50px; border-style: dashed; border-radius: 10px; border-color: black; border-width: 2px; display: inline-block; padding: 5px;">Home</span>
 
 <?php
 include "view-footer.php";
