1cb1c657b80edaac2c8bae053db14950e4bf48fa
en-OU <157330224+en-OU@users.noreply.github.com>
2024-09-16 00:22:55 -0500

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <div style="text-align: center; padding: 50px;">
-    <span style="font-size: 5px; color: black;">Fall 2024</span>
+    <span style="font-size: 10px; color: black;">Fall 2024</span>
   </div>
 </html>
 
